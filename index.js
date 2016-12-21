502f0802609ba9c3f4ada17ef8c843d9439a520d
Viljami Peltola <viljami.peltola@iki.fi>
2016-12-21 21:01:01 +0200

    THREE binding.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ module.exports = function mtlLoaderFactory(THREE){
     this.manager = ! manager ? manager : THREE.DefaultLoadingManager;
   }
 
+  THREE.MTLLoader = MTLLoader;
+
   Object.assign(MTLLoader.prototype, THREE.EventDispatcher.prototype, {
 
     /**
